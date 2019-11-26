ca497d120509b6134b8b82a31a5762abfdaba793
T.Miyake <t@miyake.me>
2019-11-27 00:30:09 +0900

    Initial commit

---
+++ Kadomatsu/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Kadomatsu
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MouseLeftButtonDown += (o, e) => DragMove();
+        }
+    }
+}
